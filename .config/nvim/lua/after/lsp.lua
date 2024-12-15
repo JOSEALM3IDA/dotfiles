f3d98c4e73cfe4a1bcc2b35ae356c291501664c3
JOSEALM3IDA <josealmeida.dev@gmail.com>
2024-12-15 21:01:29 +0100

    Change nvim lsp keybinds to Telescope

--- .config/nvim/lua/after/lsp.lua
+++ .config/nvim/lua/after/lsp.lua
@@ -19,11 +19,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = {buffer = event.buf}
 
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>Telescope lsp_declaration<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementation<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>Telescope lsp_type_definitions<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
     vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
@@ -46,8 +46,8 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp.select),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp.select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
