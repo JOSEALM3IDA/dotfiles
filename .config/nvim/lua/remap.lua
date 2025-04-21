eccf92b456ac667c4bfe4c16a07263e7490566c5
JOSEALM3IDA <josealmeida.dev@gmail.com>
2025-04-22 00:26:28 +0100

    Add neoscroll and disable pageup/pagedown

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,12 +1,12 @@
 vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false})
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 vim.g.maplocalleader="\\"
 
 -- General keymaps
 vim.keymap.set("n", "<Enter>", "o<ESC>k")
 vim.keymap.set("n", "<S-Enter>", "O<ESC>j")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
- 
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
 
@@ -25,3 +25,7 @@ vim.keymap.set('i', '<C-j>', '<Esc>jgi', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-k>', '<Esc>kgi', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-h>', '<Esc>hgi', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-l>', '<Esc>lgi', { noremap = true, silent = true })
+
+-- "Bad habits"
+vim.keymap.set('n', '<PageUp>', '<Nop>', { noremap = true, silent = true }) -- Use <C-u> / <C-b>
+vim.keymap.set('n', '<PageDown>', '<Nop>', { noremap = true, silent = true }) -- Use <C-d> / <C-f>
